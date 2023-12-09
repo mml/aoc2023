025f7951dfac0976f4afe7ce0bbf7f0f7b29d5d4
Matthew Liggett <mml@pobox.com>
2023-12-09 01:54:43 -0800

    [Kotlin] Day 9 part 1.

---
+++ 09/part1.kt
@@ -0,0 +1,50 @@
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.util.Scanner
+
+fun d(l: ArrayList<Int>): ArrayList<Int> {
+  val dl = ArrayList<Int>()
+  for (i in 1..l.count()-1) {
+    dl.add(l[i]-l[i-1])
+  }
+  return dl
+}
+
+fun zeros(l: ArrayList<Int>): Boolean {
+  for (i in l) {
+    if (i != 0) {
+      return false
+    }
+  }
+  return true
+}
+
+fun next(l: ArrayList<Int>): Int {
+  if (zeros(l)) {
+    return 0
+  } else {
+    return l.last() + next(d(l))
+  }
+}
+
+fun main() {
+  var sum = 0
+  while (true) {
+    try {
+      var line = readln()
+      var s = Scanner(line)
+      val ns = ArrayList<Int>()
+      while (s.hasNextInt()) {
+          ns.add(s.nextInt())
+      }
+      println(ns)
+      // println("->" + next(ns))
+      sum += next(ns)
+    } catch (e: Exception) {
+      println(e)
+      break
+    }
+  }
+  println(sum)
+}
